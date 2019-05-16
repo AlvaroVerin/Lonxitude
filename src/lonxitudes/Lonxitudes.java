d3d43d0632e30f4a83674db757f93bb52f916670
Martinez <martinez31199@gmail.com>
2019-05-16 09:03:45 +0200

    Modificacion

--- src/lonxitudes/Lonxitudes.java
+++ src/lonxitudes/Lonxitudes.java
@@ -2,40 +2,31 @@
 package lonxitudes;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Lonxitudes {
     public static void main(String[] args) {
         Scanner leer= new Scanner (System.in);
-        int menu = 0;
-        double km, millas, pulgada;
+        String resp;
+        String [] opciones = {"KMS", "Millas", "Pulgadas", "Salir"};
+        resp = (String) JOptionPane.showInputDialog(null, opciones);
         do {
-        System.out.println("");
-        System.out.println("Que opcion quieres?");
-        System.out.println("1. km a millas");
-               System.out.println("2. millas a km");
-               System.out.println("3. millas a pulgadas");
-               System.out.println("4. pulgadas a millas");
-               System.out.println("5. salir del programa");
-               menu=leer.nextInt();
-               
+            switch (resp) {
+                /*case "KMS":
+                  kmamillas(leer.nextDouble());
+                  break;*/
             
-        if (menu==1) {
-            System.out.println("Cuantos km son?");
-            km=leer.nextInt();
-            millas= km*0.6214;
-            System.out.println("Son "+millas+" millas");
-        }
-        if (menu==2) {
+            }
             
-        }
-        if (menu==3) {
-            
-        }
-        if (menu==4) {
-            
-        }
-         } while (menu>=1 && menu<5);   
-        System.out.println("Saliendo del programa");
+        } while (!resp.equalsIgnoreCase("Salir"));
+        
+      
     }
+    public void kmamillas (double valor){
+            double resul;
+            resul = valor * 0.6214;
+            System.out.println("El resultado es: "+resul);
+     }
+  
     
 }
